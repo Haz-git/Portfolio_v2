92b0c7fc4a5a5c6de8f6b7b86d60215a52570b26
Haz-git <zhouharry1@gmail.com>
2021-05-06 09:59:25 -0700

    Removed initial commit files, installed styled-components

--- portfolio_website/src/index.tsx
+++ portfolio_website/src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+//Components:
+import App from './components/App';
+
+//Render:
+
+render(<App />, document.getElementById('root'));
