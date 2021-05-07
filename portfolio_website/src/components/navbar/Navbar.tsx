95bd17dfdab53fbe0ce6a054c34dd5d2ed8f58e8
Haz-git <zhouharry1@gmail.com>
2021-05-07 10:23:12 -0700

    Designed navbar skeleton

--- portfolio_website/src/components/navbar/Navbar.tsx
+++ portfolio_website/src/components/navbar/Navbar.tsx
@@ -2,12 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 //Styles:
-const NavbarContainer = styled.div`
+const NavbarContainer = styled.nav`
     background: salmon;
 `;
 
+const NavbarLogoContainer = styled.div``;
+
+const NavbarLinksContainer = styled.div``;
+
+const NavbarSocialMediaContainer = styled.div``;
+
+const NavbarLinkContainer = styled.div``;
+
+const NavbarLink = styled.button``;
+
 const Navbar = () => {
-    return <NavbarContainer>Test</NavbarContainer>;
+    return (
+        <NavbarContainer>
+            <NavbarLogoContainer>
+                <div>LOGO HERE</div>
+            </NavbarLogoContainer>
+            <NavbarLinksContainer>
+                <NavbarLinkContainer>
+                    <NavbarLink>Projects</NavbarLink>
+                </NavbarLinkContainer>
+                <NavbarLinkContainer>
+                    <NavbarLink>About</NavbarLink>
+                </NavbarLinkContainer>
+                <NavbarLinkContainer>
+                    <NavbarLink>Contact</NavbarLink>
+                </NavbarLinkContainer>
+            </NavbarLinksContainer>
+            <NavbarSocialMediaContainer>
+                <div>Twitter</div>
+                <div>Github</div>
+                <div>LinkedIn</div>
+            </NavbarSocialMediaContainer>
+        </NavbarContainer>
+    );
 };
 
 export default Navbar;
