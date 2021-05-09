f24c59172f804d3d54956a33e8a19592eac9573d
Haz-git <zhouharry1@gmail.com>
2021-05-09 07:46:17 -0700

    Added button component--will refactor for reusability

---
+++ portfolio_website/src/components/hero_backdrop/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+const ButtonContainer = styled.button`
+    border: none;
+    padding: 0.8em 1em;
+    outline: none;
+    color: inherit;
+    background: #fdbc3d;
+    font-family: 'Lato', sans-serif;
+    font-size: 1.5em;
+    border-radius: 0.5em;
+    font-weight: 700;
+    box-shadow: rgba(13, 56, 72, 0.3) 0px 19px 40px,
+        rgba(13, 56, 72, 0.22) 0px 15px 12px;
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+`;
+
+const Button = () => {
+    return <ButtonContainer>Connect with me!</ButtonContainer>;
+};
+
+export default Button;
