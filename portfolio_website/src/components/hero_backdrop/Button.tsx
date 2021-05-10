119d83d8beadf232f0459faf58807e32292d63e4
Haz-git <zhouharry1@gmail.com>
2021-05-10 14:24:36 -0700

    Edited button size

--- portfolio_website/src/components/hero_backdrop/Button.tsx
+++ portfolio_website/src/components/hero_backdrop/Button.tsx
@@ -5,13 +5,13 @@ import styled from 'styled-components';
 
 const ButtonContainer = styled.button`
     border: none;
-    padding: 0.8em 1em;
+    padding: 0.7em 1em;
     outline: none;
     color: inherit;
     background: #fdbc3d;
     font-family: 'Lato', sans-serif;
     font-size: 1.5em;
-    border-radius: 0.5em;
+    border-radius: 0.3em;
     font-weight: 700;
     box-shadow: rgba(13, 56, 72, 0.3) 0px 19px 40px,
         rgba(13, 56, 72, 0.22) 0px 15px 12px;
