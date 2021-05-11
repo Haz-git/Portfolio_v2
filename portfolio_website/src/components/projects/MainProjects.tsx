bcc398ade7ce6e50bbd69950dbce83bfba749e8d
Haz-git <zhouharry1@gmail.com>
2021-05-11 15:34:30 -0700

    Designing props to pass

--- portfolio_website/src/components/projects/MainProjects.tsx
+++ portfolio_website/src/components/projects/MainProjects.tsx
@@ -19,14 +19,14 @@ const MainContainer = styled.section`
 const MainHeader = styled.h1`
     font-family: 'Lato', sans-serif;
     font-size: 2em;
-    font-weight: 700;
+    font-weight: 900;
     color: #fdbc3d;
     text-shadow: rgba(0, 0, 0, 1) 0px 3px 3px;
     text-align: center;
 
     @media ${deviceMin.mobileS} {
         padding: 0 1em;
-        font-size: 1.5em;
+        font-size: 1.3em;
     }
 `;
 
@@ -34,6 +34,13 @@ const MainProjects = () => {
     return (
         <MainContainer>
             <MainHeader>My Projects</MainHeader>
+            <ProjectItem
+                header="GymJot"
+                desc="GymJot is a passion project for lifters.GymJot is a passion
+                    project for lifters. GymJot is a passion project for
+                    lifters. GymJot is a passion project for lifters. GymJot is
+                    a passion project for lifters."
+            />
         </MainContainer>
     );
 };
