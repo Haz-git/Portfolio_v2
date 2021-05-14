00264fcbaade5ee1334d446690d18aad09bceebc
Haz-git <zhouharry1@gmail.com>
2021-05-14 06:46:34 -0700

    Attempting to conditionally render colors based on props

--- portfolio_website/src/components/hero_backdrop/Button.tsx
+++ portfolio_website/src/components/hero_backdrop/Button.tsx
@@ -3,17 +3,29 @@ import styled from 'styled-components';
 import { JsxEmit } from 'typescript';
 import { deviceMax, deviceMin } from '../../devices/breakpoints';
 
+//Interface:
+interface ButtonProps {
+    label?: string;
+    isLink?: boolean;
+    onClick?: Function;
+    buttonIcon?: JSX.Element;
+    isFilled?: boolean;
+}
+
 //Styles:
 
-const ButtonContainer = styled.button`
-    border: 2px solid #fdbc3d;
+const ButtonContainer = styled.button<ButtonProps>`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    border: 1px solid #fdbc3d;
     padding: 0.7em 1em;
     outline: none;
     color: #fdbc3d;
-    background: inherit;
+    background: ${(props) => (props.isFilled ? '#fdbc3d' : '#010d1a')};
     border-radius: 0.3em;
-    box-shadow: rgba(13, 56, 72, 0.3) 0px 19px 40px,
-        rgba(13, 56, 72, 0.22) 0px 15px 12px;
+    box-shadow: rgba(13, 56, 72, 0.3) 0px 4px 40px,
+        rgba(13, 56, 72, 0.22) 0px 4px 12px;
     cursor: pointer;
     transition: all 0.2s ease-in-out;
 
@@ -22,7 +34,7 @@ const ButtonContainer = styled.button`
     }
 
     @media ${deviceMin.mobileS} {
-        padding: 0.6em 0.9em;
+        padding: 0.7em 0.9em;
     }
 
     @media ${deviceMin.mobileM} {
@@ -52,21 +64,16 @@ const ButtonText = styled.p`
     }
 `;
 
-const IconContainer = styled.div``;
-
-//Interface:
-interface ButtonProps {
-    label?: string;
-    isLink?: boolean;
-    onClick?: Function;
-    buttonIcon?: JSX.Element;
-}
+const IconContainer = styled.div`
+    margin-right: 0.5rem;
+`;
 
 const Button = ({
     label,
     isLink,
     onClick,
     buttonIcon,
+    isFilled,
 }: ButtonProps): JSX.Element => {
     return (
         <ButtonContainer>
