21cb7b4e450cf7e6c90cd6f593daa0786940a52d
Haz-git <zhouharry1@gmail.com>
2021-05-21 13:51:55 -0700

    Adding state and styles

--- portfolio_website/src/components/aboutme/DrawerComponent.tsx
+++ portfolio_website/src/components/aboutme/DrawerComponent.tsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { deviceMin } from '../../devices/breakpoints';
 
 //Styles:
-const MainContainer = styled.div`
+const MainContainer = styled.button`
     background: #1b222a;
     padding: 1em 1em;
     border-radius: 0.5em;
+    transition: 0.2s all ease-out;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 1) 0px 3px 3px;
+
+    &:hover {
+        transform: scale(1.05);
+    }
 `;
 
-const QuestionContainer = styled.div``;
+const QuestionContainer = styled.div`
+    text-align: left;
+`;
 
-const QuestionText = styled.p``;
+const QuestionText = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    color: white;
+    text-shadow: rgba(0, 0, 0, 1) 0px 3px 3px;
+
+    @media ${deviceMin.mobileM} {
+        font-size: 1rem;
+    }
+
+    @media ${deviceMin.mobileL} {
+        font-size: 1.2rem;
+    }
+`;
 
 const AnswerContainer = styled.div`
-    display: none;
+    text-align: left;
 `;
 
-const AnswerText = styled.p``;
+const AnswerText = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    color: white;
+    text-shadow: rgba(0, 0, 0, 1) 0px 3px 3px;
+
+    @media ${deviceMin.mobileM} {
+        font-size: 1rem;
+    }
+
+    @media ${deviceMin.mobileL} {
+        font-size: 1.2rem;
+    }
+`;
 
 //Interface:
 type drawerProps = {
@@ -25,14 +61,23 @@ type drawerProps = {
 };
 
 const DrawerComponent = ({ question, answer }: drawerProps): JSX.Element => {
+    const [showAns, setShowAns] = useState(false);
+
+    const handleShowAns = () => {
+        setShowAns(true);
+    };
+
     return (
-        <MainContainer>
-            <QuestionContainer>
-                <QuestionText>{question}</QuestionText>
-            </QuestionContainer>
-            <AnswerContainer>
-                <AnswerText>{answer}</AnswerText>
-            </AnswerContainer>
+        <MainContainer onClick={handleShowAns}>
+            {showAns === false ? (
+                <QuestionContainer>
+                    <QuestionText>{question}</QuestionText>
+                </QuestionContainer>
+            ) : (
+                <AnswerContainer>
+                    <AnswerText>{answer}</AnswerText>
+                </AnswerContainer>
+            )}
         </MainContainer>
     );
 };
