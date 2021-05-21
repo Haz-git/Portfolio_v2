a708eae88e02f0b6de6ae95ba36e8f747deebf43
Haz-git <zhouharry1@gmail.com>
2021-05-21 13:59:26 -0700

    Added more elements

--- portfolio_website/src/components/aboutme/MainAboutMe.tsx
+++ portfolio_website/src/components/aboutme/MainAboutMe.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { deviceMin } from '../../devices/breakpoints';
 
 //Components:
 import { MainHeader } from '../projects/MainProjects';
@@ -9,7 +10,13 @@ import DrawerComponent from './DrawerComponent';
 const MainContainer = styled.section`
     margin-top: 5em;
     padding: 0 1em;
+
+    /* @media ${deviceMin.tablet} {
+        text-align: 
+    } */
 `;
+
+const MainDrawerContainer = styled.div``;
 
 const DrawerContainer = styled.div`
     margin: 0.5em 0;
@@ -19,30 +26,32 @@ const MainAboutMe = () => {
     return (
         <MainContainer>
             <MainHeader>Q&A: About me!</MainHeader>
-            <DrawerContainer>
-                <DrawerComponent
-                    question="Where are you based?"
-                    answer="I'm currently based in San Diego, CA."
-                />
-            </DrawerContainer>
-            <DrawerContainer>
-                <DrawerComponent
-                    question="Why did you learn code as a Microbiologist?"
-                    answer="During my microbiology career, I dabbled in bioinformatics. Through bioinformatics and my current job, I started tinkering with code more and fell in love. The rest is history!"
-                />
-            </DrawerContainer>
-            <DrawerContainer>
-                <DrawerComponent
-                    question="What are you working on right now?"
-                    answer="Currently, I'm working on finishing my master's research about insertional elements in E. coli in addition to improving my two largest personal projects-- GymJot and Petri."
-                />
-            </DrawerContainer>
-            <DrawerContainer>
-                <DrawerComponent
-                    question="Are you available for hire?"
-                    answer="I'm presently working as a Systems Integration Engineer III in my role at UCSD Research IT Services. However, I'm always open to discuss new opportunities. Don't hesitate to reach out!"
-                />
-            </DrawerContainer>
+            <MainDrawerContainer>
+                <DrawerContainer>
+                    <DrawerComponent
+                        question="Where are you based?"
+                        answer="I'm currently based in San Diego, CA."
+                    />
+                </DrawerContainer>
+                <DrawerContainer>
+                    <DrawerComponent
+                        question="Why did you learn code as a Microbiologist?"
+                        answer="During my microbiology career, I dabbled in bioinformatics. Through bioinformatics and my current job, I started tinkering with code more and fell in love. The rest is history!"
+                    />
+                </DrawerContainer>
+                <DrawerContainer>
+                    <DrawerComponent
+                        question="What are you working on right now?"
+                        answer="Currently, I'm working on finishing my master's research about insertional elements in E. coli in addition to improving my two largest personal projects-- GymJot and Petri."
+                    />
+                </DrawerContainer>
+                <DrawerContainer>
+                    <DrawerComponent
+                        question="Are you available for hire?"
+                        answer="I'm presently working as a Systems Integration Engineer III in my role at UCSD Research IT Services. However, I'm always open to discuss new opportunities. Don't hesitate to reach out!"
+                    />
+                </DrawerContainer>
+            </MainDrawerContainer>
         </MainContainer>
     );
 };
