7b43947e4ef1f207256d887abb3d1b06e3416278
Haz-git <zhouharry1@gmail.com>
2021-05-25 15:21:42 -0700

    Created component for input field

---
+++ portfolio_website/src/components/contactme/InputField.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Components:
+
+//Styles:
+
+//Render:
+
+const InputField = () => {
+    return <div></div>;
+};
+
+export default InputField;
