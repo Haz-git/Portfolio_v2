fa61530a9506cd91efe0990885df8ac5a24a030a
Haz-git <zhouharry1@gmail.com>
2021-05-26 14:23:21 -0700

    Added styles to input field

--- portfolio_website/src/components/contactme/InputField.tsx
+++ portfolio_website/src/components/contactme/InputField.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 //Components:
+const MainContainer = styled.div``;
+
+const TextInput = styled.input`
+    width: 100%;
+    padding: 0.5em 1em;
+    border-radius: 0.3em;
+    text-shadow: rgba(0, 0, 0, 1) 0px 3px 1px;
+    box-shadow: rgba(0, 0, 0, 1) 0px 3px 3px;
+    background: #1b222a;
+    color: white;
+    font-family: 'Lato', sans-serif;
+    font-weight: 700;
+    font-size: 1em;
+`;
 
 //Styles:
 
 //Render:
 
 const InputField = () => {
-    return <div></div>;
+    return (
+        <MainContainer>
+            <TextInput />
+        </MainContainer>
+    );
 };
 
 export default InputField;
