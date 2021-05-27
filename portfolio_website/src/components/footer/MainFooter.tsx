33927e86e3d9285e27e374041436dbf07c46ba19
Haz-git <zhouharry1@gmail.com>
2021-05-27 15:37:39 -0700

    Created rudimentary footer

--- portfolio_website/src/components/footer/MainFooter.tsx
+++ portfolio_website/src/components/footer/MainFooter.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 //Styles:
+const MainContainer = styled.div`
+    margin-top: 3em;
+    border-top: 1px solid #fdbc3d;
+    background: #010d1a;
+    padding: 1.5em 1em;
+    text-align: center;
+    color: #fdbc3d;
+    font-family: 'Nunito', sans-serif;
+    font-size: 0.8em;
+    font-weight: 700;
+`;
 
 //Render:
 
 const MainFooter = () => {
-    return <div>Footer</div>;
+    return (
+        <MainContainer>
+            Created by Harry Zhou <span>&copy;</span> 2021{' '}
+        </MainContainer>
+    );
 };
 
 export default MainFooter;
