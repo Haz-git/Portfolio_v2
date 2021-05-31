bd80d642f4bc015db55eeeb2574161e50fdaff67
Haz-git <zhouharry1@gmail.com>
2021-05-31 10:53:47 -0700

    Setting up input field with red border on fail

--- portfolio_website/src/components/contactme/InputField.tsx
+++ portfolio_website/src/components/contactme/InputField.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 //Components:
 const MainContainer = styled.div``;
 
-const TextInput = styled.input`
+const TextInput = styled.input<InputFieldProps>`
     width: 100%;
     padding: 0.5em 1em;
     border-radius: 0.3em;
@@ -15,6 +15,8 @@ const TextInput = styled.input`
     font-family: 'Lato', sans-serif;
     font-weight: 700;
     font-size: 1em;
+    border: ${(InputFieldProps) =>
+        InputFieldProps.inputValueError === false ? 'none' : 'red'};
 `;
 
 //Styles:
@@ -24,16 +26,23 @@ interface InputFieldProps {
     type?: string;
     onUserChange?: React.ChangeEventHandler;
     inputValue?: string;
+    inputValueError?: boolean;
 }
 
 const InputField = ({
     type,
     onUserChange,
     inputValue,
+    inputValueError,
 }: InputFieldProps): JSX.Element => {
     return (
         <MainContainer>
-            <TextInput type={type} onChange={onUserChange} value={inputValue} />
+            <TextInput
+                type={type}
+                onChange={onUserChange}
+                value={inputValue}
+                inputValueError={inputValueError}
+            />
         </MainContainer>
     );
 };
