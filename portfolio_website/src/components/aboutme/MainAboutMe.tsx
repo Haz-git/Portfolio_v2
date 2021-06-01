272275e40673c8a01f7ac8101b17171a33ffde07
Haz-git <zhouharry1@gmail.com>
2021-06-01 14:43:41 -0700

    Attached Element

--- portfolio_website/src/components/aboutme/MainAboutMe.tsx
+++ portfolio_website/src/components/aboutme/MainAboutMe.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { deviceMin } from '../../devices/breakpoints';
+import { Element } from 'react-scroll';
 
 //Components:
 import { MainHeader } from '../projects/MainProjects';
@@ -37,41 +38,43 @@ const DrawerContainer = styled.div`
 
 const MainAboutMe = () => {
     return (
-        <MainContainer>
-            <MainHeader>Q&A: About me!</MainHeader>
-            <MainDrawerContainer>
-                <DrawerContainer>
-                    <DrawerComponent
-                        question="Where are you based?"
-                        answer="I'm currently based in San Diego, CA."
-                    />
-                </DrawerContainer>
-                <DrawerContainer>
-                    <DrawerComponent
-                        question="Why did you learn to code?"
-                        answer="During my microbiology career, I dabbled in bioinformatics. Through bioinformatics and my current job, I started tinkering with code more often and fell in love. The rest is history!"
-                    />
-                </DrawerContainer>
-                <DrawerContainer>
-                    <DrawerComponent
-                        question="What are you working on right now?"
-                        answer="Currently, I'm working on finishing my master's research about insertional elements in E. coli in addition to improving my two largest personal projects-- GymJot and Petri."
-                    />
-                </DrawerContainer>
-                <DrawerContainer>
-                    <DrawerComponent
-                        question="What's important to you in tech?"
-                        answer="Growth, mentorship, and challenge are some qualities I look for in a career."
-                    />
-                </DrawerContainer>
-                <DrawerContainer>
-                    <DrawerComponent
-                        question="Are you available for hire?"
-                        answer="I'm presently working as a Systems Integration Engineer III in my role at UCSD Research IT Services. However, I'm always open to discuss new opportunities. Don't hesitate to reach out!"
-                    />
-                </DrawerContainer>
-            </MainDrawerContainer>
-        </MainContainer>
+        <Element name="aboutmecontainer">
+            <MainContainer>
+                <MainHeader>Q&A: About me!</MainHeader>
+                <MainDrawerContainer>
+                    <DrawerContainer>
+                        <DrawerComponent
+                            question="Where are you based?"
+                            answer="I'm currently based in San Diego, CA."
+                        />
+                    </DrawerContainer>
+                    <DrawerContainer>
+                        <DrawerComponent
+                            question="Why did you learn to code?"
+                            answer="During my microbiology career, I dabbled in bioinformatics. Through bioinformatics and my current job, I started tinkering with code more often and fell in love. The rest is history!"
+                        />
+                    </DrawerContainer>
+                    <DrawerContainer>
+                        <DrawerComponent
+                            question="What are you working on right now?"
+                            answer="Currently, I'm working on finishing my master's research about insertional elements in E. coli in addition to improving my two largest personal projects-- GymJot and Petri."
+                        />
+                    </DrawerContainer>
+                    <DrawerContainer>
+                        <DrawerComponent
+                            question="What's important to you in tech?"
+                            answer="Growth, mentorship, and challenge are some qualities I look for in a career."
+                        />
+                    </DrawerContainer>
+                    <DrawerContainer>
+                        <DrawerComponent
+                            question="Are you available for hire?"
+                            answer="I'm presently working as a Systems Integration Engineer III in my role at UCSD Research IT Services. However, I'm always open to discuss new opportunities. Don't hesitate to reach out!"
+                        />
+                    </DrawerContainer>
+                </MainDrawerContainer>
+            </MainContainer>
+        </Element>
     );
 };
 
