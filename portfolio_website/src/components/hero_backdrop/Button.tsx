8c670dbe30e738724e586e96179c8447653d613b
Haz-git <zhouharry1@gmail.com>
2021-06-01 12:56:22 -0700

    Added isDisabled and onDisabledText prop

--- portfolio_website/src/components/hero_backdrop/Button.tsx
+++ portfolio_website/src/components/hero_backdrop/Button.tsx
@@ -2,36 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import { deviceMax, deviceMin } from '../../devices/breakpoints';
 
-//Interface:
-interface ButtonProps {
-    label?: string;
-    isLink?: boolean;
-    onClick?: React.MouseEventHandler;
-    buttonIcon?: JSX.Element;
-    btnBackground?: string;
-    btnTextColor?: string;
-}
-
 //Styles:
 
 const ButtonContainer = styled.button<ButtonProps>`
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    border: 1px solid #fdbc3d;
+    border: ${(props) =>
+        props.disabled
+            ? '1px solid rgba(255,255,255,0.8)'
+            : '1px solid #fdbc3d'};
     padding: 0.7em 1em;
     outline: none;
-    color: ${(props) => props.btnTextColor};
-    background: ${(props) => props.btnBackground};
+    color: ${(props) =>
+        props.disabled ? 'rgba(255,255,255,0.8)' : props.btnTextColor};
+    background: ${(props) =>
+        props.disabled ? '#1b222a' : props.btnBackground};
     border-radius: 0.3em;
     box-shadow: rgba(13, 56, 72, 0.3) 0px 4px 40px,
-        f rgba(13, 56, 72, 0.22) 0px 4px 12px;
-    cursor: pointer;
+        rgba(13, 56, 72, 0.22) 0px 4px 12px;
+    cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
     transition: all 0.2s ease-in-out;
     width: 100%;
 
     &:hover {
-        transform: scale(1.05);
+        transform: ${(props) => (props.disabled ? 'none' : 'scale(1.05)')};
     }
 
     @media ${deviceMin.mobileS} {
@@ -69,6 +64,18 @@ const IconContainer = styled.div`
     margin-right: 0.5rem;
 `;
 
+//Interface:
+interface ButtonProps {
+    label?: string;
+    isLink?: boolean;
+    onClick?: React.MouseEventHandler;
+    buttonIcon?: JSX.Element;
+    btnBackground?: string;
+    btnTextColor?: string;
+    isDisabled?: boolean;
+    onDisabledText?: string;
+}
+
 const Button = ({
     label,
     isLink,
@@ -76,15 +83,22 @@ const Button = ({
     buttonIcon,
     btnBackground,
     btnTextColor,
+    isDisabled,
+    onDisabledText,
 }: ButtonProps): JSX.Element => {
     return (
         <ButtonContainer
             btnBackground={btnBackground}
             btnTextColor={btnTextColor}
             onClick={onClick}
+            disabled={isDisabled}
         >
             {buttonIcon && <IconContainer>{buttonIcon}</IconContainer>}
-            <ButtonText>{label}</ButtonText>
+            {isDisabled === false ? (
+                <ButtonText>{label}</ButtonText>
+            ) : (
+                <ButtonText>{onDisabledText}</ButtonText>
+            )}
         </ButtonContainer>
     );
 };
